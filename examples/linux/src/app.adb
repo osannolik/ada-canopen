b57c290e8c85f49785b77d2cd9b7d6ab69a877c9
osannolik <osannolik@godtycklig.se>
2018-08-24 17:29:08 +0200

    Update linux example

--- examples/linux/src/app.adb
+++ examples/linux/src/app.adb
@@ -1,18 +1,14 @@
 with Ada.Real_Time; use Ada.Real_Time;
 with ACO.Drivers.Socket;
-with ACO.Messages;
 with ACO.Nodes;
 with ACO.States;
 with ACO.OD;
 with ACO.Log;
 with Ada.Text_IO.Text_Streams;
-with ACO.OD_Types;
 with ACO.OD.Example;
-
 
 with Ada.Text_IO;
 with Ada.Exceptions;
-with ACO.OD_Types.Entries;
 
 package body App is
    use Ada.Text_IO;
@@ -32,71 +28,7 @@ package body App is
    procedure Run
    is
       Next_Release : Time := Clock;
-
-      --  Msg_Rx : ACO.Messages.Message;
-      Msg_Tx : ACO.Messages.Message :=
-         ACO.Messages.Create (CAN_Id => 16#1#,
-                              RTR    => False,
-                              Data   => (1, 2, 3, 4));
-      X : ACO.Messages.Data_Type := 10;
-
-      use type ACO.Messages.Data_Type;
-
    begin
-
-      declare
-
---           Obj_Ref : ACO.OD_Types.Object_Ref := O.Get_Object_Ref (16#1008#);
---
---           Obj : ACO.OD_Types.Object_Base'Class := O.Get_Object (16#1008#);
---
---           Bulle_Ref : ACO.OD_Types.Entry_Ref := O.Get_Entry_Ref (16#1008#, 0);
-
-         Bulle : ACO.OD_Types.Entry_Base'Class := O.Get_Entry (16#1008#, 0);
-
-         --  V : constant ACO.OD_Types.Entries.F32 := ACO.OD_Types.Entries.F32 (Bulle);
-
-         V2 : ACO.OD_Types.Byte_Array := Bulle.Read;
-
-         S : constant ACO.OD_Types.Entries.Visible_String :=
-            ACO.OD.Example.Device_Name_Entry'Class (Bulle).Read;
-
-         --         pragma Unreferenced (Obj, Obj_Ref, Bulle_Ref, Bulle, V2, S);
-
-         U : constant ACO.OD_Types.Entries.Entry_U32 :=
-            ACO.OD_Types.Entries.Create (ACO.OD_Types.WO, 1337);
-
-         UPre : ACO.OD_Types.Entry_Base'Class := O.Get_Entry (16#1005#, 0);
-
-         CHBT : constant Natural := O.Get_Heartbeat_Consumer_Period (1);
-
-         pragma Unreferenced (UPre, CHBT);--, S);
-
-         Hbt_Per : Natural;
-      begin
-
-         Hbt_Per := O.Get_Heartbeat_Consumer_Period (4);
-
-         Hbt_Per := 2 * Hbt_Per;
-
-         O.Set_Heartbeat_Consumer_Period (4, Hbt_Per);
-
-         Hbt_Per := O.Get_Heartbeat_Consumer_Period (4);
-
-         V2 (V2'Last) := V2 (V2'First);
-
-         Bulle.Write (V2);
-
-         O.Set_Entry (Bulle, 16#1008#, 0);
-
-         O.Set_Entry (U, 16#1005#, 0);
-         UPre := O.Get_Entry (16#1005#, 0);
-
-         Ada.Text_IO.Put_Line (S);
-      end;
-
-
-
       ACO.Log.Set_Stream (Text_Streams.Stream (Current_Output));
       ACO.Log.Set_Level (ACO.Log.Debug);
 
@@ -105,16 +37,6 @@ package body App is
       N.Set_State (ACO.States.Initializing);
 
       loop
-         --  D.Send_Message (Msg_Tx);
-
-         if X >= 20 then
-            X := 10;
-         else
-            X := X + 1;
-         end if;
-
-         Msg_Tx.Data (Msg_Tx.Data'First) := X;
-
          Next_Release := Next_Release + Milliseconds (500);
          delay until Next_Release;
       end loop;
