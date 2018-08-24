3c92222e72793d5234435ff8bf4ec02a8bb5914f
osannolik <osannolik@godtycklig.se>
2018-08-24 22:06:07 +0200

    Alignment fix of conversion?

--- src/aco-generic_entry_types.adb
+++ src/aco-generic_entry_types.adb
@@ -1,3 +1,5 @@
+with Ada.Unchecked_Conversion;
+
 package body ACO.Generic_Entry_Types is
 
    function Read (This : Entry_Type) return Item_Type is
@@ -27,20 +29,20 @@ package body ACO.Generic_Entry_Types is
 
    function Convert (Data : Item_Type) return Byte_Array
    is
-      Bytes : constant Byte_Array (0 .. Item_Type'Size / 8 - 1);
+      Bytes : constant Byte_Array (0 .. Data'Size / 8 - 1);
       for Bytes'Address use Data'Address;
       pragma Import (Convention => Ada, Entity => Bytes);
    begin
       return Bytes;
    end Convert;
 
-   function Convert (Bytes : Byte_Array) return Item_Type
-   is
-      Data : constant Item_Type;
-      for Data'Address use Bytes'Address;
-      pragma Import (Convention => Ada, Entity => Data);
+   function Convert (Bytes : Byte_Array) return Item_Type is
+      --  Might get alignment issues?
+      function To_Item is new Ada.Unchecked_Conversion
+         (Source => Byte_Array,
+          Target => Item_Type);
    begin
-      return Data;
+      return To_Item (Bytes);
    end Convert;
 
    function "=" (L : Entry_Type; R : Item_Type) return Boolean is
--- src/aco-od_types-entries.ads
+++ src/aco-od_types-entries.ads
@@ -15,7 +15,7 @@ package ACO.OD_Types.Entries is
    type Entry_U8 is new U8_Pack.Entry_Type with null record;
 
 
-   subtype U16 is Interfaces.Unsigned_16;
+   type U16 is new Interfaces.Unsigned_16 with Alignment => 1;
 
    package U16_Pack is new ACO.Generic_Entry_Types (U16);
 
@@ -24,14 +24,16 @@ package ACO.OD_Types.Entries is
 
 --   type U32 is new Natural range 0 .. 2**32-1 with Size => 32;
 
-   subtype U32 is Interfaces.Unsigned_32;
+   type U32 is new Interfaces.Unsigned_32 with Alignment => 1;
 
    package U32_Pack is new ACO.Generic_Entry_Types (U32);
 
    type Entry_U32 is new U32_Pack.Entry_Type with null record;
 
 
-   package F32_Pack is new ACO.Generic_Entry_Types (IEEE_Float_32);
+   type F32 is new IEEE_Float_32;
+
+   package F32_Pack is new ACO.Generic_Entry_Types (F32);
 
    type Entry_F32 is new F32_Pack.Entry_Type with null record;
 
